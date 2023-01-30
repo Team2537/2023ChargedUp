7ca276a08a1d8201cb1538a83c45471820320ff9
Falon <47285315+Tatiman-Dev@users.noreply.github.com>
2023-01-30 10:51:39 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/subsystems/GripperSubsystem.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.constants.Constants.*;
+import static frc.robot.constants.Ports.*;
+
+public class GripperSubsystem extends SubsystemBase{
+
+    public GripperSubsystem(){
+        // Gripper Subsystem constructor function
+    }
+
+    // Place subsystem functions below this line please - falon
+    
+}
