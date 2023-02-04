621b2e4583fe029dd9dfe1212f6d16ff26d1bf6d
mteam88 <matthew8edelen@gmail.com>
2023-02-04 12:50:29 -0500

    splice lidar sensor code into gripper code

--- src/main/java/frc/robot/subsystems/GripperSubsystem.java
+++ src/main/java/frc/robot/subsystems/GripperSubsystem.java
@@ -6,14 +6,22 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.util.function.Function;
+
 // import revlib spark max motor
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 // import revlib encoder
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+// import lidar tools
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycle;
 
 public class GripperSubsystem extends SubsystemBase {
+  private static final double FILTERED_GAIN = 0;
   private SparkMaxPIDController m_pidController;
   private RelativeEncoder m_encoder;
   private double kP;
@@ -23,11 +31,19 @@ public class GripperSubsystem extends SubsystemBase {
   private double kFF;
   private double kMaxOutput;
   private double kMinOutput;
+  private DigitalOutput m_trig;
+  private double m_target_high;
+  private double m_target_low;
+  private DigitalInput m_read;
+  private DutyCycle m_pwm;
+  private boolean m_vibrate;
+  private final Function<Double, Boolean> m_rumble;
 
-  public GripperSubsystem(int deviceID) {
+  public GripperSubsystem(int deviceID, double target_low, double target_high, Function<Double, Boolean> rumble, int lidar_read_port, int lidar_trigger_port) {
     // initialize motor
     m_motor = new CANSparkMax(deviceID, MotorType.kBrushless);
     pid = m_motor.getPIDController();
+    m_rumble = rumble;
 
     /**
      * The RestoreFactoryDefaults method can be used to reset the configuration
@@ -36,7 +52,7 @@ public class GripperSubsystem extends SubsystemBase {
      * these
      * parameters will not persist between power cycles
      */
-    //m_motor.restoreFactoryDefaults();
+    // m_motor.restoreFactoryDefaults();
 
     /**
      * In order to use PID functionality for a controller, a SparkMaxPIDController
@@ -76,11 +92,32 @@ public class GripperSubsystem extends SubsystemBase {
     SmartDashboard.putNumber("Feed Forward", kFF);
     SmartDashboard.putNumber("Max Output", kMaxOutput);
     SmartDashboard.putNumber("Min Output", kMinOutput);
+
+    // init lidar
+    m_target_low = target_low;
+    m_target_high = target_high;
+    /*
+     * m_trig = DIOJNI.initializeDIOPort(HAL.getPort((byte) 0), true);
+     * DIOJNI.setDIODirection(m_trig, false);
+     * DIOJNI.setDIO(m_trig,false);
+     */
+
+    m_trig = new DigitalOutput(lidar_trigger_port);
+    m_trig.set(false);
+
+    /*
+     * m_read = DIOJNI.initializeDIOPort(HAL.getPort((byte) 1), true);
+     * DIOJNI.setDIODirection(m_read, true);
+     */
+    m_read = new DigitalInput(lidar_read_port);
+    m_pwm = new DutyCycle(m_read);
   }
 
   SparkMaxPIDController pid;
   CANSparkMax m_motor;
   private double target;
+  private int m_count;
+  private double filtered_pulse_width;
 
   /*
    * Moves arm to position based on angleDef.
@@ -114,6 +151,28 @@ public class GripperSubsystem extends SubsystemBase {
 
     SmartDashboard.putNumber("Target", target);
     SmartDashboard.putNumber("ProcessVariable", m_encoder.getVelocity());
+
+    // get and output the pulse width of m_read
+    // System.out.println(DIOJNI.getDIO(m_read));
+    boolean inTarget = isTarget();
+    // only trigger the sensor every 10th time
+    if (m_count++ % 10 == 0 && m_vibrate) {
+      m_rumble.apply(inTarget ? 1.0 / 4.0 : 0.0);
+    } else if (m_count % 10 == 0) {
+      m_rumble.apply(0.0);
+    }
+  }
+
+  // implement a low pass filter
+  private double lowPass(double raw, double filtered, double alpha) {
+    return (1 - alpha) * filtered + alpha * (raw - filtered);
+  }
+
+  // return true if the target is in target bounds
+  public boolean isTarget() {
+    double pulse_width = m_pwm.getHighTimeNanoseconds() * 10e-5;
+    filtered_pulse_width = lowPass(pulse_width, filtered_pulse_width, FILTERED_GAIN);
+    return filtered_pulse_width > m_target_low && filtered_pulse_width < m_target_high;
   }
 
   @Override
