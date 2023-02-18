66d902568a2162805635e58278068790204a3c56
Falon <47285315+Tatiman-Dev@users.noreply.github.com>
2023-02-18 10:40:12 -0500

    Added shuffleboard tab

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,9 +1,14 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class VisionSubsystem extends SubsystemBase{
     public VisionSubsystem(){
-        
-    }
+        ShuffleboardTab visionTab = Shuffleboard.getTab("Vision Tab");
+     }
 }
