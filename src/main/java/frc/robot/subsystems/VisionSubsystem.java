4cea9fb499aca014c86c569d8b5f1793332a580c
Enter-D-Username <117410470+Enter-D-Username@users.noreply.github.com>
2023-02-18 11:26:48 -0500

    updated computer vision to actually work

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -6,37 +6,67 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.util.datalog.BooleanLogEntry;
 import edu.wpi.first.util.datalog.DoubleLogEntry;
 import edu.wpi.first.wpilibj.DataLogManager;
 
 public class VisionSubsystem extends SubsystemBase{
 
-    private static BooleanLogEntry BLog;
-    private static DoubleLogEntry DLog;
+// Defines the logs
+    private static DoubleLogEntry XOffsetLog;
+    private static DoubleLogEntry YOffsetLog;
+    private static DoubleLogEntry AreaLog;
+    private static DoubleLogEntry SkewLog;
 
+//Defines the sensor readings
     private double x;
     private double y;
     private double area;
+    private double skew;
+
+    private NetworkTableEntry tx;
+    private NetworkTableEntry ty;
+    private NetworkTableEntry ta;
+    private NetworkTableEntry ts;
 
     public VisionSubsystem(){
+
+//Creates the network tables for use
+        NetworkTable limeTable = NetworkTableInstance.getDefault().getTable("limelight");
+        tx = limeTable.getEntry("tx");
+        ty = limeTable.getEntry("ty");
+        ta = limeTable.getEntry("ta");
+        ts = limeTable.getEntry("ts");
+
+// creates the logs
+        XOffsetLog = new DoubleLogEntry(DataLogManager.getLog(), "/VisionSubsystem/XOffset");
+        YOffsetLog = new DoubleLogEntry(DataLogManager.getLog(), "/VisionSubsystem/YOffset");
+        AreaLog = new DoubleLogEntry(DataLogManager.getLog(), "/VisionSubsystem/AreaLog");
+        SkewLog = new DoubleLogEntry(DataLogManager.getLog(), "/VisionSubsystem/SkewLog");
+
+// creates a shuffleboard tab for this subsystem
         ShuffleboardTab visionTab = Shuffleboard.getTab("Vision Tab");
 
+// creates the collums for the shuffleboard
         visionTab.addNumber("X Offset", () -> x);
         visionTab.addNumber("Y Offset", () -> y);
         visionTab.addNumber("Area", () -> area);
+        visionTab.addNumber("Skew", () -> skew);
      }
 
      @Override
      public void periodic() {
-        NetworkTable limeTable = NetworkTableInstance.getDefault().getTable("limelight");
-        NetworkTableEntry tx = limeTable.getEntry("tx");
-        NetworkTableEntry ty = limeTable.getEntry("ty");
-        NetworkTableEntry ta = limeTable.getEntry("ta");
 
-        x = tx.getDouble(0.0);
-        y = ty.getDouble(0.0);
-        area = ta.getDouble(0.0);
+// continually puts the values in the log
+        XOffsetLog.append(x);
+        YOffsetLog.append(y);
+        AreaLog.append(area);
+        SkewLog.append(skew);
+
+// continually updates the network tables
+        skew = ts;
+        x = tx;
+        y = ty;
+        area = ta;
 
      }
    
