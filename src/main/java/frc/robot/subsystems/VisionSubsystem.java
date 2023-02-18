88089f10af1e3fdc7ee0986911d31dfdb3f5da24
Enter-D-Username <117410470+Enter-D-Username@users.noreply.github.com>
2023-02-18 10:44:25 -0500

    Began logging for vision subsystem

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -6,8 +6,14 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.util.datalog.BooleanLogEntry;
+import edu.wpi.first.util.datalog.DoubleLogEntry;
+import edu.wpi.first.wpilibj.DataLogManager;
 
 public class VisionSubsystem extends SubsystemBase{
+
+    private static BooleanLogEntry BLog;
+  private static DoubleLogEntry DLog;
     public VisionSubsystem(){
         ShuffleboardTab visionTab = Shuffleboard.getTab("Vision Tab");
      }
