518eff9e4fcb6c94ffce7023a0940a13fd61bcb2
Micah Rao <angelina425@hotmail.com>
2023-03-04 11:06:14 -0500

    add SetChassisState command from setHeading branch and schedule it

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,6 +20,7 @@ import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.IOConstants;
 import frc.robot.commands.SwerveTeleopCommand;
+import frc.robot.commands.SetChassisStateCommand;
 import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -67,41 +68,43 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // 1. Create trajectory settings
-    TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
-      AutoConstants.kMaxSpeedMps,
-      AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-              .setKinematics(DriveConstants.kDriveKinematics);
+//     // 1. Create trajectory settings
+//     TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
+//       AutoConstants.kMaxSpeedMps,
+//       AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+//               .setKinematics(DriveConstants.kDriveKinematics);
 
-// 2. Generate trajectory
-Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
-      new Pose2d(0, 0, new Rotation2d(0)),
-      List.of(
-              new Translation2d(1, 0),
-              new Translation2d(1, -1)),
-      new Pose2d(2, -1, Rotation2d.fromDegrees(180)),
-      trajectoryConfig);
+// // 2. Generate trajectory
+// Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+//       new Pose2d(0, 0, new Rotation2d(0)),
+//       List.of(
+//               new Translation2d(1, 0),
+//               new Translation2d(1, -1)),
+//       new Pose2d(2, -1, Rotation2d.fromDegrees(180)),
+//       trajectoryConfig);
 
-// 3. Define PID controllers for tracking trajectory
-PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
-PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
-ProfiledPIDController thetaController = new ProfiledPIDController(
-      AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
-thetaController.enableContinuousInput(-Math.PI, Math.PI);
+// // 3. Define PID controllers for tracking trajectory
+// PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
+// PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
+// ProfiledPIDController thetaController = new ProfiledPIDController(
+//       AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+// thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
-// 4. Construct command to follow trajectory
-SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
-      trajectory,
-      swerveSubsystem::getPose,
-      DriveConstants.kDriveKinematics,
-      xController,
-      yController,
-      thetaController,
-      swerveSubsystem::setModuleStates,
-      swerveSubsystem);
+// // 4. Construct command to follow trajectory
+// SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+//       trajectory,
+//       swerveSubsystem::getPose,
+//       DriveConstants.kDriveKinematics,
+//       xController,
+//       yController,
+//       thetaController,
+//       swerveSubsystem::setModuleStates,
+//       swerveSubsystem);
 
-// 5. Add some init and wrap-up, and return everything
-return new SequentialCommandGroup(new InstantCommand(() -> swerveSubsystem.resetOdometry(trajectory.getInitialPose())), swerveControllerCommand,  new InstantCommand(() -> swerveSubsystem.stopModules()));
-  }
+// // 5. Add some init and wrap-up, and return everything
+// return new SequentialCommandGroup(new InstantCommand(() -> swerveSubsystem.resetOdometry(trajectory.getInitialPose())), swerveControllerCommand,  new InstantCommand(() -> swerveSubsystem.stopModules()));
+// 
+return new SetChassisStateCommand(swerveSubsystem, 0.0, 0.0, 90.0);
+}
 
 }
--- src/main/java/frc/robot/commands/SetChassisStateCommand.java
+++ src/main/java/frc/robot/commands/SetChassisStateCommand.java
@@ -13,29 +13,43 @@ import java.util.function.Supplier;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
-public class SetChassisState extends CommandBase {
+public class SetChassisStateCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final SwerveSubsystem mSwerveSubsystem;
   private final double xSpdFunction, ySpdFunction, heading;
   private final PIDController pidController;
-  private final double kp=0.6, ki=0.0, kd=0.0;
 
+  //private final double kp=0.1, ki=0.0, kd=0.0; //turn smoothly but oscillates at setpoint
+  //private final double kp=0.03, ki=0.1, kd=0.0; //turn smoothly but oscillates at setpoint
+  //private final double kp=0.01, ki=0.0, kd=0.0; //barely turns
+  //private final double kp=0.05, ki=0.0, kd=0.0; //turn smoothly but oscillates at setpoint
+  //private final double kp=0.05, ki=0.0, kd=0.01; //turn smoothly but oscillates at setpoint
+  private final double kp=0.05, ki=0.0, kd=0.0;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public SetChassisState(SwerveSubsystem swerveSubsystem,  double xSpdFunction, double ySpdFunction, double heading) {
+  public SetChassisStateCommand(SwerveSubsystem swerveSubsystem,  double xSpdFunction, double ySpdFunction, double heading) {
     mSwerveSubsystem = swerveSubsystem;
     this.xSpdFunction = xSpdFunction;
     this.ySpdFunction = ySpdFunction;
     this.heading = heading;
     pidController=new PIDController(kp, ki, kd);
-    pidController.enableContinuousInput(0.0, 360.0);
+    pidController.enableContinuousInput(-360,360);
+
+    ShuffleboardTab tab = Shuffleboard.getTab("Swerve State");
+
+    tab.addNumber("turning speed", () -> turningSpeed);
+    tab.addNumber("desired heading", () -> heading);
+    tab.addNumber("our heading", () -> swerveSubsystem.getHeading());
 
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(swerveSubsystem);
@@ -45,13 +59,16 @@ public class SetChassisState extends CommandBase {
   @Override
   public void initialize() {}
 
+  double turningSpeed = 0;
+
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     double xSpeed = xSpdFunction;
     double ySpeed = ySpdFunction;
-    double turningSpeed;
-    turningSpeed = pidController.calculate(mSwerveSubsystem.getHeading(), heading);
+    
+    turningSpeed = -pidController.calculate(mSwerveSubsystem.getHeading(), heading);
+    //SmartDashboard.putNumber("Turning speed", turningSpeed);
 
     ChassisSpeeds chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
       xSpeed, ySpeed, turningSpeed, mSwerveSubsystem.getRotation2d());
