fc561a322b88a759ca6cfe8396ecfbe74de91b00
Falon <47285315+Tatiman-Dev@users.noreply.github.com>
2023-03-04 10:25:04 -0500

    Removed unnecessary logging

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -13,18 +13,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * The ExampleSubsystem class is a simple example of a subsystem.
  */
 public class ExampleSubsystem extends SubsystemBase {
-  private final DoubleLogEntry logEntry;
+  //private final DoubleLogEntry logEntry;
 
   /** Creates a new ExampleSubsystem. */
   public ExampleSubsystem() {
     //log example things
-    logEntry = new DoubleLogEntry(DataLogManager.getLog(), "/subsystem_name/encoders");
+    //logEntry = new DoubleLogEntry(DataLogManager.getLog(), "/subsystem_name/encoders");
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    logEntry.append(0);
+    //logEntry.append(0);
   }
 
   @Override
