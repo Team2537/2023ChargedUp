7ad8e2f0cfa669184795411fb0400ca288bd4937
Micah Rao <49841276+jstN0body@users.noreply.github.com>
2023-03-07 18:04:55 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -98,7 +98,8 @@ public final class Constants {
         public static final double kTeleAngularMaxSpeedRps = kPhysicalMaxAngularSpeedRps;
         public static final double kTeleDriveMaxAccelerationUnitsPerSecond = kPhysicalMaxAccelerationMps;
         public static final double kTeleDriveMaxAngularAccelerationUnitsPerSecond = kPhysicalMaxAngularAccelerationRps;
-        
+	
+	public static final double kSpeedMultiplier = 1.0;
         
         public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
                 new Translation2d(kWheelBaseMeters / 2, kTrackWidthMeters / 2), //mFrontLeft
