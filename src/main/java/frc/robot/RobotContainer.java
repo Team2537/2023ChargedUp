15063135971cb8f7949274566303422eb6dfed5c
Micah Rao <49841276+jstN0body@users.noreply.github.com>
2023-03-07 19:59:27 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -31,7 +31,6 @@ import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.SetColorCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.RGBSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 
