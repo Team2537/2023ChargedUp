d0d84d5e99c72d6e19f7b00cc1a55be57e788c88
Micah Rao <angelina425@hotmail.com>
2023-03-12 08:13:15 -0400

    add center balance path and lock for auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -190,19 +190,19 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        PathPlannerTrajectory trajectory = PathPlanner.loadPath("New Path",
+        PathPlannerTrajectory trajectory = PathPlanner.loadPath("CenterBalance",
                 new PathConstraints(AutoConstants.kMaxSpeedMps, AutoConstants.kMaxAccelerationMetersPerSecondSquared));
        
 
-        // return new PathCommand(m_swerveSubsystem, trajectory);
+        return new PathCommand(m_swerveSubsystem, trajectory).andThen(new LockCommand(m_swerveSubsystem));
 
-        return new HomingCommand(m_armPivotSubsystem, m_armTelescopeSubsystem).andThen(
-                new FixedAngleCommand(m_armPivotSubsystem, 15.66)).andThen(
-                new FixedExtensionCommand(m_armTelescopeSubsystem, 6.4)).andThen(
-                new OpenGripperCommand(m_gripperSubsystem)).andThen(
-                new WaitCommand(0.5)).andThen(
-                new HomingCommand(m_armPivotSubsystem, m_armTelescopeSubsystem).alongWith(
-                new PathCommand(m_swerveSubsystem, trajectory)));
+        // return new HomingCommand(m_armPivotSubsystem, m_armTelescopeSubsystem).andThen(
+        //         new FixedAngleCommand(m_armPivotSubsystem, 15.66)).andThen(
+        //         new FixedExtensionCommand(m_armTelescopeSubsystem, 6.4)).andThen(
+        //         new OpenGripperCommand(m_gripperSubsystem)).andThen(
+        //         new WaitCommand(0.5)).andThen(
+        //         new HomingCommand(m_armPivotSubsystem, m_armTelescopeSubsystem).alongWith(
+        //         new PathCommand(m_swerveSubsystem, trajectory)));
 
         // // 1. Create trajectory settings
         // TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
