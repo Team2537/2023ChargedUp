eb408b80cb4598868b91eee5e19caa16562d1aff
Micah Rao <angelina425@hotmail.com>
2023-03-14 20:30:51 -0400

    create SwerveHomingCommand

--- src/main/java/frc/robot/commands/SwerveHomingCommand.java
+++ src/main/java/frc/robot/commands/SwerveHomingCommand.java
@@ -1,21 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands;
 
+import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 /** An example command that uses an example subsystem. */
-public class ZeroHeadingCommand extends InstantCommand {
+public class SwerveHomingCommand extends InstantCommand {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final SwerveSubsystem m_subsystem;
+  private final SwerveSubsystem mSwerveSubsystem;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ZeroHeadingCommand(SwerveSubsystem subsystem) {
-    m_subsystem = subsystem;
+  public SwerveHomingCommand(SwerveSubsystem subsystem) {
+    mSwerveSubsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
@@ -23,7 +28,10 @@ public class ZeroHeadingCommand extends InstantCommand {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_subsystem.zeroHeading();
+    mSwerveSubsystem.getFrontLeftModule().resetEncoders();
+    mSwerveSubsystem.getFrontRightModule().resetEncoders();
+    mSwerveSubsystem.getBackLeftModule().resetEncoders();
+    mSwerveSubsystem.getBackRightModule().resetEncoders();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
