18cb58316de89aa685d729cee61ac38a2cf98e92
Falon <47285315+Tatiman-Dev@users.noreply.github.com>
2023-05-02 13:36:48 -0400

    Modified docs

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"frc.robot.subsystems"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"frc.robot"},{"l":"frc.robot.subsystems"}];updateSearchResults();
