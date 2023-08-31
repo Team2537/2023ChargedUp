6fa5c7a6c01ea792daea23ddf2651477345eb1d6
Micah Rao <raoboys12@gmail.com>
2023-08-31 19:41:03 -0400

    vision stuff

--- src/main/java/frc/robot/commands/GampieceAlignCommand.java
+++ src/main/java/frc/robot/commands/GampieceAlignCommand.java
@@ -4,19 +4,24 @@ import java.lang.ModuleLayer.Controller;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
 
 public class GampieceAlignCommand extends CommandBase {
 
 
     private VisionSubsystem m_subsystem;
+    private SwerveSubsystem m_swerve;
 
     private PIDController m_PIDcontroller;
 
-    public GampieceAlignCommand(VisionSubsystem subsystem) {
+    private double targetOffset = 0.0;
+
+    public GampieceAlignCommand(VisionSubsystem subsystem, SwerveSubsystem swerve_subsystem) {
         // Use addRequirements() here to declare subsystem dependencies.
         m_subsystem = subsystem;
-        addRequirements(subsystem);
+        m_swerve = swerve_subsystem;
+        addRequirements(subsystem, swerve_subsystem);
 
         m_PIDcontroller = new PIDController(0.0, 0.0, 0.0);
     
@@ -27,7 +32,9 @@ public class GampieceAlignCommand extends CommandBase {
   
     // Called every time the scheduler runs while the command is scheduled.
     @Override
-    public void execute() {}
+    public void execute() {
+      
+    }
   
     // Called once the command ends or is interrupted.
     @Override
