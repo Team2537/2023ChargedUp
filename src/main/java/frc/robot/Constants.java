4ff49ea0387be1cc93f8fb1849fbde301f2bd89e
Falon <47285315+falOn-Dev@users.noreply.github.com>
2023-10-25 23:12:35 -0400

    reset PID stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,9 +46,9 @@ public final class Constants {
     public static final class Auton
     {
 
-        public static final PIDFConfig xAutoPID     = new PIDFConfig(0.7, 0, 0);
-        public static final PIDFConfig yAutoPID     = new PIDFConfig(0.3, 0, 0.2);
-        public static final PIDFConfig angleAutoPID = new PIDFConfig(0.3, 0, 0.05);
+        public static final PIDFConfig xAutoPID     = new PIDFConfig(0.3, 0, 0);
+        public static final PIDFConfig yAutoPID     = new PIDFConfig(0.3, 0, 0);
+        public static final PIDFConfig angleAutoPID = new PIDFConfig(0.3, 0, 0);
 
         public static final PIDConstants translationConstants = new PIDConstants(Auton.yAutoPID.p, Auton.yAutoPID.i, Auton.yAutoPID.d);
         public static final PIDConstants rotationConstants = new PIDConstants(Auton.angleAutoPID.p, Auton.angleAutoPID.i, Auton.angleAutoPID.d);
